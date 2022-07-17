a11143d976b98ab14edf71f95facf20080410b67
gdutchak <galyna.dutchak99@gmail.com>
2022-07-17 20:33:54 +0300

    update task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,10 +9,11 @@ function createGallery(image) {
   return image.map(({ preview, original, description }) => {
     return `<a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" 
-  alt="${description}" title="${description}"/>
+  alt="${description}"/>
 </a>` }).join('')
 }
 
 const lightbox = new SimpleLightbox('.gallery a', {
-
+  captionsData: "alt",
+  captionsDelay: 250,
 });
